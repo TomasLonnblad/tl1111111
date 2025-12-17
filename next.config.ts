05699914b7c70d6be4755c24ca52264877247ada
Tomas <tomaslonnblad@gmail.com>
2025-12-17 18:03:50 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV = 'production';
 const nextConfig = {
   basePath: isProd ? '/nextjs-blog-deployment' : '',
   output: 'export',
-  distDir: 'dist',
+ 
   images: {
     unoptimized: true,
  },
