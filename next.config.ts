159a0a2c69c9a806c934e3e94f0932f476acef04
Tomas <tomaslonnblad@gmail.com>
2025-12-17 18:07:07 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
-const isProd = process.env.NODE_ENV = 'production';
-const nextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-
- 
-  images: {
-    unoptimized: true,
- },
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
-
+export default nextConfig;
